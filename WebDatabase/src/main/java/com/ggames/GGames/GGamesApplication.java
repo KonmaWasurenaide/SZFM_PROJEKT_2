2f9be01121a0213b88c88f009a1d2a419ad9cb19
FazakasMatyas <fazakasmatyi044@gmail.com>
2025-10-01 16:55:35 +0200

    Database setup

---
+++ WebDatabase/src/main/java/com/ggames/GGames/GGamesApplication.java
@@ -0,0 +1,13 @@
+package com.ggames.GGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GGamesApplication.class, args);
+	}
+
+}
---
+++ WebDatabase/src/test/java/com/ggames/GGames/GGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggames.GGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

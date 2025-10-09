671dfa26f98f599ad3ea8ce2c1f7e6cff5aaccbb
Artiwastaken <adambornemissza@gmail.com>
2025-10-09 18:57:39 +0200

    Basic UI completed for login/register

---
+++ WebDatabase/src/main/resources/static/kuspgames.js
@@ -0,0 +1,12 @@
+const loginButton = document.getElementById('loginbutton');
+const registerButton = document.getElementById('registerbutton');
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('passwordInput');
+
+//Register function
+registerButton.addEventListener('click', async () => {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+
+});

0be5d7e2ae9888d00a2cc7bf8d5e7883b8e477b9
KonmaWasurenaide <kozicz04@gmail.com>
2025-11-07 20:10:36 +0100

    Crash sound FX was implemented

---
+++ Assets/Scripts/CrashManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (!col.gameObject.CompareTag("Ground"))
+        {
+            audioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
